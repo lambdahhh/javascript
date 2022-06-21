3b2e3163aa30f031cec6085b741f6fa7bc0eafe4
pvolkov <p.volkov@ndv.ru>
2022-06-21 15:53:13 +0300

    every func fix

--- src/components/array/every.js
+++ src/components/array/every.js
@@ -2,7 +2,7 @@ console.log('Array every');
 
 const myAwesomeArray = ["a", "b", "c", "d", "e"];
 
-Array.prototype.mySome = function (callback) {
+Array.prototype.myEvery = function (callback) {
     let result = true;
     for(let i = 0; i < this.length; i++) {
         result = callback(this[i], i, this);
@@ -17,9 +17,9 @@ const res = myAwesomeArray.every((item, index, arr) => {
     return item.length === 1
 })
 
-const res2 = myAwesomeArray.mySome((item, index, arr) => {
+const res2 = myAwesomeArray.myEvery((item, index, arr) => {
     return item.length === 1
 })
 
 console.log('every', res);
-console.log('mySome', res2);
+console.log('myEvery', res2);
